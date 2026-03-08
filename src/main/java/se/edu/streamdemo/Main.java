1ad8c69fcc22527b1364e887a99fc372bcafa2ed
sushmiithaa <rsushmiithaa@gmail.com>
2026-03-08 18:50:36 +0800

    Use lambdas to do some filtering operations

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,6 +6,8 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,9 +21,13 @@ public class Main {
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlinesUsingStreams(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        System.out.println();
+        ArrayList<Task> filteredList = filteredList(tasksData,"11");
+        printAllData(filteredList);
     }
 
     private static void printWelcomeMessage() {
@@ -50,6 +56,20 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks){
+        System.out.println("Print deadlines using streams");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((t1,t2) -> t1.getDescription().compareToIgnoreCase(t2.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filteredList(ArrayList<Task> tasks, String filterString){
+        ArrayList<Task> filterList = (ArrayList<Task>) tasks.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filterList;
     }
 
 }
