77ec0cac6d24c994a41cdff8e38d2ed4996ef050
sushmiithaa <rsushmiithaa@gmail.com>
2026-03-08 18:16:44 +0800

    Use welcome message

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task manager (using streams)");
         Datamanager dataManager = new Datamanager("./data/data.txt");
+        printWelcomeMessage();
         ArrayList<Task> tasksData = dataManager.loadData();
 
         System.out.println("Printing all data ...");
